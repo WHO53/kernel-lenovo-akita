e6e97c7bac9d7b5f7f5091ff8f868f5878c1db8d
Chao Yu <yuchao0@huawei.com>
2018-07-27 18:15:16 +0800

    f2fs: fix to spread clear_cold_data()
    [ Upstream commit 2baf07818549c8bb8d7b3437e889b86eab56d38e ]
    
    We need to drop PG_checked flag on page as well when we clear PG_uptodate
    flag, in order to avoid treating the page as GCing one later.
    
    Signed-off-by: Weichao Guo <guoweichao@huawei.com>
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -2837,6 +2837,13 @@ void f2fs_invalidate_page(struct page *page, unsigned int offset,
 	}
 
 	clear_cold_data(page);
+<<<<<<< HEAD
+=======
+
+	/* This is atomic written page, keep Private */
+	if (IS_ATOMIC_WRITTEN_PAGE(page))
+		return;
+>>>>>>> 02fe0b2173 (f2fs: fix to spread clear_cold_data())
 
 	if (IS_ATOMIC_WRITTEN_PAGE(page))
 		return f2fs_drop_inmem_page(inode, page);
@@ -2855,7 +2862,12 @@ int f2fs_release_page(struct page *page, gfp_t wait)
 		return 0;
 
 	clear_cold_data(page);
+<<<<<<< HEAD
 	f2fs_clear_page_private(page);
+=======
+	set_page_private(page, 0);
+	ClearPagePrivate(page);
+>>>>>>> 02fe0b2173 (f2fs: fix to spread clear_cold_data())
 	return 1;
 }
 
